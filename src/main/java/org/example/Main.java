b16204de1899567af18da1364e3c72ed93092b0d
Okman-0920 <nohoksun94@gmail.com>
2024-11-19 15:51:30 +0900

    addWhile

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         App app = new App();
@@ -12,9 +14,19 @@ public class Main {
 class App {
 
     public void run() {
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("== 명언 앱 ==");
 
-        System.out.print("명령) ");
+        while(true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine();
 
+            if (cmd.equals("종료")) { //문자열은 == 으로 안됨
+                break;
+            }
+        }
+        scanner.close();
     }
 }
