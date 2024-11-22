cb7307160c6d1324c33b95ba2224c27c1b844783
Okman-0920 <nohoksun94@gmail.com>
2024-11-22 15:18:04 +0900

    step5_action add, list

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,20 +6,24 @@ public class Main {
     public static void main(String[] args) {
         App app = new App();
         app.run();
-        // new 다음은 class가 나와야한다
     }
 }
 
 class App {
+    Scanner scanner;
+    int lastId;
+    WiseSaying[] wiseSayings;
+    int wiseSayingsSize;
+
+    App() {
+        scanner = new Scanner(System.in);
+        lastId = 0;
+        wiseSayings = new WiseSaying[100];
+        wiseSayingsSize = 0;
+    }
 
     public void run() {
-
-        Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
-        int lastId = 0; // 마지막으로 등록된 ID
-//        WiseSaying lastWiseSaying = null;
-        WiseSaying[] wiseSayings = new WiseSaying[100];
-        int wiseSayingsSize = 0; // 데이터가 0개 있다, 배열에 index라고 알면된다
 
         while(true) {
             System.out.print("명령) ");
@@ -28,34 +32,38 @@ class App {
             if (cmd.equals("종료")) {
                 break;
             } else if(cmd.equals("등록")) {
-                System.out.print("작가:");
-                String author = scanner.nextLine();
-
-                System.out.print("명언:");
-                String content = scanner.nextLine();
-
-                int id = ++lastId;
-
-                WiseSaying wiseSaying = new WiseSaying(id, content, author);
-                wiseSayings[wiseSayingsSize] = wiseSaying;
-                wiseSayingsSize++;
-
-//                System.out.println(Arrays.toString(wiseSayings)); // 확인코드
-
-                System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
-                // 내 코드: System.out.println(id + "번 명언이 등록되었습니다.");
+                actionAdd();
 
             } else if (cmd.equals("목록")) {
-                System.out.println("번호 / 작가 / 명언\n" + "----------------------");
-                                // 이렇게 만들 경우, 가장 마지막에 저장된 1개만 온다. 단 저장안되면 실행오류남
-
-                for (WiseSaying wiseSaying : wiseSayings) {
-                     if (wiseSaying == null) break;
-                    System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.author, wiseSaying.content));
-                }
+                actionList();
             }
         }
         scanner.close();
+    }
+
+    private void actionAdd() {
+        System.out.print("작가:");
+        String author = scanner.nextLine();
+
+        System.out.print("명언:");
+        String content = scanner.nextLine();
+
+        int id = ++lastId;
+
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+        wiseSayings[wiseSayingsSize] = wiseSaying;
+        wiseSayingsSize++;
+
+        System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
+    }
+
+    private void actionList() {
+        System.out.println("번호 / 작가 / 명언\n" + "----------------------");
+
+        for (WiseSaying wiseSaying : wiseSayings) {
+            if (wiseSaying == null) break; // if절 {}안에서 break 앞에 내용이 없을 경우 생략 가능하다
+            System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.author, wiseSaying.content));
+        }
     }
 }
 
