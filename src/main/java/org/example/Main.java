2bc401c4c3ce53d358e0753883683c7af2608779
Okman-0920 <nohoksun94@gmail.com>
2024-11-22 10:13:44 +0900

    using try-catch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,33 +17,42 @@ class App {
         Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
         int lastId = 0; // 마지막으로 등록된 ID
-        WiseSaying lastWiseSaying; // 위치를 소환할 수 있는 변수 작성 (기억하도록)
+        WiseSaying lastWiseSaying = null;
 
         while(true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
 
-            if (cmd.equals("등록")) {
-                System.out.print("명언 :");
+            if (cmd.equals("종료")) {
+                break;
+            } else if(cmd.equals("등록")) {
+                System.out.print("명언:");
                 String content = scanner.nextLine();
 
-                System.out.print("작가 :");
+                System.out.print("작가:");
                 String author = scanner.nextLine();
-                int id = ++lastId; // 이해를 돕기위해 강사님과 동일한 지역변수명으로 변경
+                int id = ++lastId;
 
                 WiseSaying wiseSaying = new WiseSaying(id, content, author);
 
                 lastWiseSaying = wiseSaying;
 
-
-                // 만약에 여기에 int reg = 1;을 선언하면 계속 반복이라 +2만될꺼
                 System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
                 // 내 코드: System.out.println(id + "번 명언이 등록되었습니다.");
 
-            } else if (cmd.equals("목록")) { //문자열은 == 으로 안됨
+            } else if (cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언\n" + "----------------------");
+                                // 이렇게 만들 경우, 가장 마지막에 저장된 1개만 온다. 단 저장안되면 실행오류남
 
-            } else if (cmd.equals("종료")) { //문자열은 == 으로 안됨
-                break;
+                try { // try-catch 일단 사용
+                    System.out.println("%d / %s / %s".formatted(lastWiseSaying.id, lastWiseSaying.author, lastWiseSaying.content));
+                } catch (NullPointerException e) {
+                    System.out.println("등록된 명언이 없습니다. 새로운 명언을 등록해주세요.");
+                }
+
+                /*if (lastWiseSaying == null) { //if문으로 오류 대응
+                    System.out.println("등록된 명언이 없습니다. 새로운 명언을 등록해주세요.");
+                }*/
             }
         }
         scanner.close();
@@ -61,3 +70,4 @@ class WiseSaying {
         this.author = author;
     }
 }
+
